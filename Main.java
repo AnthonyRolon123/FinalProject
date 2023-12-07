678de8ef5bbd4a3a305d4a564bb2598a92f8fb51
AnthonyRolon123 <anthonyrolon1234@gmail.com>
2023-12-07 17:55:02 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,13 +2,22 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        int n = 100;
-
-        System.out.println("The value for 10 is: " + Fib(n));
+        int n = 45; // Number of terms to print
+        int[] memo = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            System.out.println(Fib(i, memo) + " ");
+        }
     }
 
-    public static int Fib(int n) {
-        if(n ==1){return 1;}
-        return n * Fib(n-1);
+    static int Fib(int n, int[] memo){
+        if (memo[n] != 0)
+            return memo[n];
+        if (n == 1 || n == 2)
+            return 1;
+        else {
+            memo[n] = Fib(n - 1, memo)
+                    + Fib(n - 2, memo);
+            return memo[n];
+        }
     }
 }
